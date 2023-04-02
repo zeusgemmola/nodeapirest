a3c193d19d56387e38a894071e4ce8efd307859a
Samuel Gomez <contact@samuelgomez.fr>
2023-04-02 19:27:41 +0200

    feat(yup): validate data

--- products/controller.js
+++ products/controller.js
@@ -1,4 +1,12 @@
+const { object, string, number } = require("yup");
 const Products = require("./model");
+
+let productSchema = object({
+  title: string().required(),
+  price: number().required().positive().integer(),
+  stock: number().positive().integer(),
+  id: number().required().positive().integer(),
+});
 
 const read = (db) => async (req, res) => {
   try {
@@ -13,14 +21,20 @@ const read = (db) => async (req, res) => {
 const create = (db) => async (req, res) => {
   const { body } = req; // on imagine que côté front, un formulaire envoie ces données en post
 
-  const product = new Products({ ...body }); // on créé un instance de notre modèle
-
   try {
-    const collection = db.collection("products");
-    const result = await collection.insertOne(product);
-    res.status(200).json(result);
-  } catch (error) {
-    res.status(500, err);
+    const parsedProducts = productSchema.cast({ ...body });
+
+    const product = new Products({ ...parsedProducts }); // on créé un instance de notre modèle
+
+    try {
+      const collection = db.collection("products");
+      const result = await collection.insertOne(product);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500, err);
+    }
+  } catch (err) {
+    res.status(400).json({ message: "Erreur de format", err });
   }
 };
 
