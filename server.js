67fcf4900842dfddc29ceceb84838e28d4bf6d2d
Samuel Gomez <contact@samuelgomez.fr>
2023-04-02 15:40:23 +0200

    feat(products): display list

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const { connexion } = require('./config/db');
+const { connexion } = require("./config/db");
 
 const PORT = 3000;
 
-connexion.then(async db => {
+connexion.then(async (db) => {
   const app = express();
+
+  app.get("/products", async (req, res) => {
+    const collection = db.collection("products");
+    const list = await collection.find({}).toArray();
+    res.json(list);
+  });
 
   app.listen(PORT, () => {
     console.log(`le serveur est lancé sur le port ${PORT}`);
   });
-
 });
-
